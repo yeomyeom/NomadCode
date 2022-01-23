5826f44f275de4d5e13eae39a28d3005564e8e51
yeomyeom <ysy9639@naver.com>
2022-01-24 00:10:17 +0900

    #1.4~1.8

--- yoom/src/public/js/app.js
+++ yoom/src/public/js/app.js
@@ -1,2 +1,46 @@
 /* 여기의 socket은 서버를 의미함 */
-const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+const socket = new WebSocket(`ws://${window.location.host}`);//소캣 열고
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("connected to server");
+});//브라우저와 서버가 연결 되었을때
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);//메시지를 화면에 ul li m.data로 보여줌
+
+});//서버로부터 브라우저가 데이터를 받았을때
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from server");
+});//서버가 죽었을때
+/*
+setTimeout(() =>{
+    socket.send("hello from browser!");
+}, 10000);//브라우저에서 서버로 메시지 보냄
+*/
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    //socket.send(input.value);//브라우저에서 작성한 내용을 서버로 전송
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    //socket.send(input.value);//message인지 nick 인지 구분이 안감
+    socket.send(makeMessage("nickname", input.value));
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- yoom/src/server.js
+++ yoom/src/server.js
@@ -1,3 +1,8 @@
+/*
+socket == 브라우저
+ws == 웹 소켓 서버
+ws.on("connect, 함수") == 각 브라우저와 연결된 서버 동작 정의
+*/
 import express from "express"
 import http from "http"
 import WebSocket from "ws"
@@ -16,13 +21,42 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 and w
 
 const server = http.createServer(app);//http 서버 생성
 const wss = new WebSocket.Server({server});//webSoket 서버 
-
+/*
 function handleConnetion(socket){//socket은 연결된 브라우저를 의미
     console.log(socket);
 }
-
 wss.on("connection", handleConnetion);
 //연결이벤트, function, socket:메시지 주고받는 통로(객체) backend에 연결된 사람의 정보(브라우저) 제공해줌
+*/
 
+const sockets = [];//socket(개별 브라우저)들 모아두는 DB
+
+
+wss.on("connection", (socket) => {//socket 객체를 통해 누가 접속했는지 암 wss 객체는 웹소켓 서버 전체를 의미
+    sockets.push(socket);//브라우저 정보 모아두기(다른 소켓(브라우저)에 메시지 보내주기 위함)
+    socket["nickname"] = "Anonymous";//socket 객체에 key값 추가 가능함(로그인할때 닉네임 안정한 사람은 Ann으로 뜨게)
+    console.log("connected to server");
+    socket.on("close", () => console.log("disconnected from browser"));//브라우저가 꺼졌는지 추적
+    /*
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });//브라우저가 서버로 메시지를 보냈을때 listener
+    */
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);//메시지 타입 구분 가능(별명, 메시지)
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname} : ${message.payload.toString()}`));//DB에 있는 모든 소캣을 aSocket에 담고 send 함(브토드케스트)
+            case "nickname":
+                socket["nickname"] = message.payload;//socket객체에 nickname key를 추가해 브라우저로부터 온 message.payload를 쓴다.
+                //message = {type:별명, 메시지, payload: 데이터}
+                //nickname을 지정하면 Anonymous 에서 payload로 덮어씌워지기 됨 socket객체(브라우저)가 disconnect 되지 않는한 유지(disconnect 하면 sockets 배열에서 빼줘야할거 같은데)
+        }
+        //socket.send(message.toString());
+    });//브라우저가 서버로 메시지를 보냈을때 listener, 브라우저 메시지 그대로 다시 브라우저로, 서버 - 브라우저 1:1 관계만 연결되어있음
+
+    socket.send("hello");//브라우저로 hello 메시지 보냄
+});//매번 새로운 브라우저가 서버로 접속했을때 각 브라우저에 이 동작을 함(이벤트 리스너 주르륵)
+//이 코드를 안해줘도 됨 기본적으로 on connection 이 정의되어있음
 server.listen(3000, handleListen);
 
