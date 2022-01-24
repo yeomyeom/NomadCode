514e500df71d7762caa85bbc61b8b8269d30b02e
yeomyeom <ysy9639@naver.com>
2022-01-24 22:44:53 +0900

    #1.9~2.3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NomadCode
 NomadCode zoom clone coding
+
+## Requirements
+1. NodeJs
+2. npm i nodemon -D
+3. npm i @babel/core @babel/cli @babel/node @babel/preset-env -D
+4. npm i express
+5. npm i pug
+6. npm i ws
+7. npm i socket.io
--- yoom/src/public/js/app.js
+++ yoom/src/public/js/app.js
@@ -1,4 +1,5 @@
 /* 여기의 socket은 서버를 의미함 */
+/*
 const messageList = document.querySelector("ul");
 const messageForm = document.querySelector("#message");
 const nickForm = document.querySelector("#nick");
@@ -23,17 +24,14 @@ socket.addEventListener("message", (message) => {
 socket.addEventListener("close", () => {
     console.log("Disconnected from server");
 });//서버가 죽었을때
-/*
-setTimeout(() =>{
-    socket.send("hello from browser!");
-}, 10000);//브라우저에서 서버로 메시지 보냄
-*/
 
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
     //socket.send(input.value);//브라우저에서 작성한 내용을 서버로 전송
     socket.send(makeMessage("new_message", input.value));
+    li.innerText = `You ${input.value}`;
+    messageList.append(li);
     input.value = "";
 }
 function handleNickSubmit(event){
@@ -44,3 +42,24 @@ function handleNickSubmit(event){
 }
 messageForm.addEventListener("submit", handleSubmit);
 nickForm.addEventListener("submit", handleNickSubmit);
+*/
+
+const socket = io();//브라우저에서 서버로 연결 console에서 io 입력하면 함수 구현 코드가 보임
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg){
+    console.log(`backend says : ` + msg);
+}
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", {payload:input.value}, backendDone);//emit(event이름, dataobject(string이 아니여도 됨))
+    //함수는 반드시 마지막 인자로 와야함 정의는 브라우저(프론트) 인자는 서버(백엔드)에서 줄 수 있음
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
